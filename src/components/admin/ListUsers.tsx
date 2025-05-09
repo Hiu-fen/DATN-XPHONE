34228403e867c0fef405c5a67e39caf5d2e0e6ff
Hiu-fen <phamhieu03112005@gmail.com>
2025-05-09 16:48:04 +0700

    mHiiiii

--- src/components/admin/ListUsers.tsx
+++ src/components/admin/ListUsers.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 import { User } from '../../interface/user'
 import { useNavigate } from 'react-router-dom'
 
-// hiiiii ////// update
+<h1>hiiiiiiii</h1>
 const GetUser = () => {
   const nav = useNavigate();
 
