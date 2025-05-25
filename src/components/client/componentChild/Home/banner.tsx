920189e545d60095a0ac7156229308d8afb69088
cuan1210 <quannhph50104@gmail.com>
2025-05-25 17:20:37 +0700

    xây dựng giao diện Home 2

--- src/components/client/componentChild/Home/banner.tsx
+++ src/components/client/componentChild/Home/banner.tsx
@@ -1,7 +1,7 @@
 const BannerClient = () => {
   return (
     <>
-      <div className="overflow-hidden relative">
+      <div className="mx-4 overflow-hidden relative">
         <img
           src="./src/assets/banner.webp"
           alt="Banner"
