a520956c4ea879f17da4083fada57349340e77d0
hiu-fen <phamhieu03112005@gmail.com>
2025-07-04 19:00:49 +0700

    Revert "thêm địa chỉ đơn vị vận chuyển"

--- backend/models/address.js
+++ backend/models/address.js
@@ -8,18 +8,8 @@ const addressSchema = new mongoose.Schema({
   },
   name: { type: String, required: true },
   phone: { type: String, required: true },
-  address: { type: String, required: true }, // ví dụ: "Số 1 Nguyễn Huệ"
+  address: { type: String, required: true },
   isDefault: { type: Boolean, default: false },
-
-  // GHN fields
-  province_id: { type: Number, required: true },
-  district_id: { type: Number, required: true },
-  ward_code: { type: String, required: true },
-
-  // Optional - chỉ để hiển thị
-  province_name: { type: String },
-  district_name: { type: String },
-  ward_name: { type: String },
 });
 
 module.exports = mongoose.model("Address", addressSchema);
